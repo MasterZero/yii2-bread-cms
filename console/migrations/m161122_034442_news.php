a288433ea8acef7aea4f9f84f64ac3de986e844c
MasterZero <sergey.sv5@mail.ru>
2017-01-13 13:18:13 +0600

    first application

---
+++ console/migrations/m161122_034442_news.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m161122_034442_news extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m161122_034442_news cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
---
+++ console/migrations/m161122_034518_category.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m161122_034518_category extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m161122_034518_category cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
